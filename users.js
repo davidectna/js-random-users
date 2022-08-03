fac9dc15d2b84310d8804b3f13f77686dfb73908
davidectna@gmail.com <INDIRIZZO_E-MAIL>
2022-08-03 17:49:24 +0200

    javascript part

---
+++ users.js
@@ -0,0 +1,35 @@
+const addBtn = document.getElementById('Add-Button');
+addBtn.addEventListener('click',addUser);
+
+function addUser(){
+axios.get('https://randomuser.me/api')
+.then(function(response){
+    console.log(response.data.results);
+    const userImg = response.data.results[0].picture.medium ;
+    const userName = response.data.results[0].name.first;
+    const userSurname = response.data.results[0].name.last;
+
+    createCard (userImg,userName,userSurname);
+})
+.catch(function(error) {
+    console.log(error)
+})
+}
+
+function createCard(image,name,surname) {
+
+    let images = document.createElement('img');
+    images.src= image;
+    images.className = 'img-fluid';
+
+    let completeName = document.createElement('h4');
+    name.innerText = name + '' + surname;
+
+    let  card = document.createElement('div');
+    card.className = "col-4";
+    card.appendChild(images);
+    card.appendChild(completeName);
+
+    let galleryElement = document.getElementById('gallery');
+    galleryElement.appendChild(card);
+}
